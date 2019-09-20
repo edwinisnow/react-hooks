03b75532007fe1b7951598b2fa4f9fb6b9516e82
edwinisnow <edwin04@gmail.com>
2019-09-20 06:50:10 +0100

    use state

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 function App() {
   return (
     <div className="App">
-
+      <HookCounter />
+      {/* <ClassCounter /> */}
     </div>
   );
 }
