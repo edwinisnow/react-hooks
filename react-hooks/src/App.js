18ac5f90497ec5cdf81ebfaf942f6e87e55e7d28
edwinisnow <edwin04@gmail.com>
2019-09-21 08:50:27 +0100

    useEffect Run effects only once

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -8,11 +8,15 @@ import HookCounterThree from './components/HookCounterThree';
 import HookCounterFour from './components/HookCounterFour';
 import ClassCounterOne from './components/ClassCounterOne';
 import HookCounterOne from './components/HookCounterOne';
+import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterOne />
+      <HookMouse />
+      {/* <ClassMouse /> */}
+      {/* <HookCounterOne /> */}
       {/* <ClassCounterOne /> */}
       {/* <HookCounterFour /> */}
       {/* <HookCounterThree /> */}
