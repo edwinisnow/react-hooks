34a0c50ea7a975682acaac7f175eb32a0267a364
edwinisnow <edwin04@gmail.com>
2019-09-21 08:15:36 +0100

    useEffect Hook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -6,11 +6,15 @@ import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterFour from './components/HookCounterFour';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterFour />
+      <HookCounterOne />
+      {/* <ClassCounterOne /> */}
+      {/* <HookCounterFour /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounter /> */}
