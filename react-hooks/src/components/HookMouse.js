990994bdce6ac1b1ebed0355c17b708b68759ae2
edwinisnow <edwin04@gmail.com>
2019-09-25 06:30:16 +0100

    useEffect with cleanup

--- react-hooks/src/components/HookMouse.js
+++ react-hooks/src/components/HookMouse.js
@@ -14,6 +14,11 @@ function HookMouse() {
     useEffect(() => {
         console.log('UseEffect called')
         window.addEventListener('mousemove', logMousePosition)
+
+        return () => {
+            console.log('Component unmounting code')
+            window.removeEventListener('mousemove', logMousePosition)
+        }
     }, [])
 
 
